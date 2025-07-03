3c20f3a99b05265ef5b635da1499785132ee9568
Rzza <rzzawm@proton.me>
2025-07-03 10:43:00 +0330

    Revert "Merge branch 'feat/timer' into develop"
    This reverts commit 66420939813eab61b004bceef91f7f3591bbf29e, reversing
    changes made to 86181d8cefa4c0df2bbf033220bdd8970df6ba59.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { DirectionProvider } from '@radix-ui/react-direction';
 import './global.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <DirectionProvider dir="rtl">
-            <App />
-        </DirectionProvider>
+        <App />
     </StrictMode>,
 );

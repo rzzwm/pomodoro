d2decf76db2eeab986f40bc98dcb0d5956fffbd1
Rzza <rzzawm@proton.me>
2025-07-04 17:10:46 +0330

    feat: timer ui re-design

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,73 +1,55 @@
+import { useEffect, useRef, useState } from 'react';
+import { Play, Pause, RotateCcw } from 'lucide-react';
 import { type Stages } from '@/lib/types';
-import { useEffect, useState } from 'react';
 import { useTimer } from '@/lib/hooks';
 import { usePrefs } from '@/components/prefs-provider';
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
+import { Button } from './ui/button';
 
 export default function Timer() {
     const [stage, setStage] = useState<Stages>('POMO');
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setStage(event.target.value as Stages);
+    const handleChange = (value: Stages) => {
+        setStage(value);
     };
     return (
-        <div className="flex gap-4">
-            <RadioButtonGroup value={stage} onChange={handleChange} />
+        <div className="flex flex-col items-center justify-center gap-6">
             <TimerProgress stage={stage} />
+            <StagePicker value={stage} onChange={handleChange} />
         </div>
     );
 }
 
-function RadioButtonGroup({
+function StagePicker({
     onChange: handleChange,
     value: selectedValue,
 }: {
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    value: string;
+    onChange: (value: Stages) => void;
+    value: Stages;
 }) {
+    const STAGE_DETAILS: Record<Stages, { label: string }> = {
+        POMO: { label: 'پومودورو' },
+        BRAKE: { label: 'استراحت کوتاه' },
+        L_BRAKE: { label: 'استراحت طولانی' },
+    };
     return (
-        <div>
-            <h2>حالت تایمر</h2>
-            <div>
-                <label>
-                    <input
-                        type="radio"
-                        name="myRadioGroup"
-                        value="option1"
-                        checked={selectedValue === 'option1'}
-                        onChange={handleChange}
-                    />
-                    حالت اول
-                </label>
-            </div>
-
-            <div>
-                <label>
-                    <input
-                        type="radio"
-                        name="myRadioGroup"
-                        value="option2"
-                        checked={selectedValue === 'option2'}
-                        onChange={handleChange}
-                    />
-                    حالت دوم
-                </label>
-            </div>
-
-            <div>
-                <label>
-                    <input
-                        type="radio"
-                        name="myRadioGroup"
-                        value="option3"
-                        checked={selectedValue === 'option3'}
-                        onChange={handleChange}
-                    />
-                    حالت سوم
-                </label>
-            </div>
-            <p>
-                حالت تایمر: <strong>{selectedValue}</strong>
-            </p>
-        </div>
+        <ToggleGroup
+            type="single"
+            variant="outline"
+            value={selectedValue}
+            onValueChange={handleChange}
+        >
+            {Object.keys(STAGE_DETAILS).map(stage => (
+                <ToggleGroupItem
+                    className="transition-colors"
+                    key={stage}
+                    value={stage}
+                >
+                    <span className="px-2">
+                        {STAGE_DETAILS[stage as Stages].label}
+                    </span>
+                </ToggleGroupItem>
+            ))}
+        </ToggleGroup>
     );
 }
 
@@ -86,6 +68,8 @@ function TimerProgress({ stage }: { stage: Stages }) {
     const { timeLeft, timerState, pause, reset, start } = useTimer({
         totalSeconds: currentStage.totalSeconds,
     });
+    const timeLeftPercent = (timeLeft / currentStage.totalSeconds) * 100;
+    const formattedTimeLeft = `${String(Math.floor(timeLeft / 60)).padStart(2, '0')} : ${String(timeLeft % 60).padStart(2, '0')}`;
 
     useEffect(() => {
         reset();
@@ -96,18 +80,84 @@ function TimerProgress({ stage }: { stage: Stages }) {
     }
 
     return (
-        <div>
-            <progress
-                value={timeLeft}
-                max={currentStage.totalSeconds}
-            ></progress>
-            <span>{timeLeft}</span>
-            <div>
-                <button onClick={toggleTimer}>
-                    {timerState === 'STARTED' ? 'نگهش دار' : 'شروع کن'}
-                </button>
-                <button onClick={() => reset()}>از اول</button>
+        <div className="space-y-6">
+            <CircularProgressBar
+                progress={timeLeftPercent}
+                text={formattedTimeLeft}
+            />
+            <div className="flex justify-center gap-4">
+                <Button
+                    variant="outline"
+                    onClick={toggleTimer}
+                    className="size-12 rounded-full"
+                >
+                    {timerState === 'STARTED' ? (
+                        <Pause className="size-6" />
+                    ) : (
+                        <Play className="size-6" />
+                    )}
+                </Button>
+                <Button
+                    variant="outline"
+                    onClick={() => reset()}
+                    className="size-12 rounded-full active:*:-rotate-90"
+                >
+                    <RotateCcw className="size-6 transition-transform" />
+                </Button>
             </div>
         </div>
+    );
+}
+
+function CircularProgressBar({
+    progress,
+    text,
+}: {
+    progress: number;
+    text: string;
+}) {
+    const strokeWidth = 5;
+    const radius = 50 - strokeWidth / 2;
+    const circumference = 2 * Math.PI * radius;
+    const dashoffset = useRef(circumference);
+    const clampedProgress = Math.max(0, Math.min(100, progress));
+    const newDashoffset =
+        circumference - (clampedProgress / 100) * circumference;
+    dashoffset.current = newDashoffset;
+
+    return (
+        <svg className="w-full" viewBox="0 0 100 100">
+            <circle
+                cx="50"
+                cy="50"
+                r={radius}
+                fill="none"
+                className="stroke-accent"
+                strokeWidth={strokeWidth - 2.5}
+            />
+            <circle
+                cx="50"
+                cy="50"
+                r={radius}
+                fill="none"
+                strokeWidth={strokeWidth}
+                className="stroke-primary transition-[stroke_dasharray] duration-500"
+                strokeLinecap="round"
+                transform="rotate(-90 50 50)"
+                strokeDasharray={circumference}
+                strokeDashoffset={dashoffset.current}
+            />
+            <text
+                direction="ltr"
+                className="fill-primary"
+                fontWeight={900}
+                x="50%"
+                y="50%"
+                textAnchor="middle"
+                dominantBaseline="middle"
+            >
+                {text}
+            </text>
+        </svg>
     );
 }

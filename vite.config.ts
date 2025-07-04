6916f95a4d73979126486db9c74f88e930aedf24
Rzza <rzzawm@proton.me>
2025-07-04 21:42:56 +0330

    change base path for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: '/pomodoro/',
     resolve: {
         alias: {
             '@': resolve(__dirname, 'src'),
